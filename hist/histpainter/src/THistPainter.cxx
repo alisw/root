beae49e6924e984c3ce07e81f25e0a6ea72fb0f2
Dario Berzano <dario.berzano@cern.ch>
2015-06-27 16:45:07 +0200

    Fixing some FPEs in THistPainter and TMinuit

--- hist/histpainter/src/THistPainter.cxx
+++ hist/histpainter/src/THistPainter.cxx
@@ -4724,7 +4724,8 @@ void THistPainter::PaintColorLevels(Option_t *)
    }
    Int_t ndivz  = TMath::Abs(ndiv);
    if (fH->TestBit(TH1::kUserContour) == 0) fH->SetContour(ndiv);
-   Double_t scale = ndivz/dz;
+   Double_t scale = 1;
+   if (dz) scale = ndivz/dz;
 
    Int_t color;
    TProfile2D* prof2d = dynamic_cast<TProfile2D*>(fH);
