3e5d7bc3abaac26f0a9d396b7783e0fbc27d9fec
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-09-23 12:48:48 +0200

    Revert "[ci] Add GSL options"
    This reverts commit e01a2243bfde765019a957af72f21d7870fc3c55.

--- .github/workflows/root-ci-config/build_root.py
+++ .github/workflows/root-ci-config/build_root.py
@@ -75,6 +75,9 @@ def main():
         **load_config(f'{this_script_dir}/buildconfig/{args.platform}.txt')
     }
 
+    if args.binaries:
+        options_dict = remove_gpl_options(options_dict)
+
     options = build_utils.cmake_options_from_dict(options_dict)
 
     if WINDOWS:
@@ -211,6 +214,13 @@ def parse_args():
 
 def print_trace():
     build_utils.log.print()
+
+def remove_gpl_options(options_dict: dict):
+    gpl_options = ['builtin_fftw3', 'builtin_gsl', 'builtin_unuran', 'fftw3', 'mathmore', 'pythia8', 'unuran']
+    for opt in gpl_options:
+        options_dict[opt] = 'off'
+    return options_dict
+
 
 @github_log_group("Clean up from previous runs")
 def cleanup_previous_build():
