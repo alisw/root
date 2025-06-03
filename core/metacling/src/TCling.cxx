0a57bf4ed77bc31b3e0d6a63edff3a89a22c387f
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-06-03 23:57:25 +0200

    Revert "[TCling] Fix suppression of enum forward declarations"
    This reverts commit 9d2f7612a6e88d3a7148b19e131402349bac509b.

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -2171,10 +2171,8 @@ void TCling::RegisterModule(const char* modulename,
                   }
                }
                if (scopes.empty() || DC) {
-                  // We know the scope; let's look for the enum. For that, look
-                  // for the *last* closing parentheses of an attribute because
-                  // there can be multiple.
-                  size_t posEnumName = fwdDeclsLine.rfind("\"))) ");
+                  // We know the scope; let's look for the enum.
+                  size_t posEnumName = fwdDeclsLine.find("\"))) ", 32);
                   R__ASSERT(posEnumName != std::string::npos && "Inconsistent enum fwd decl!");
                   posEnumName += 5; // skip "\"))) "
                   while (isspace(fwdDeclsLine[posEnumName]))
